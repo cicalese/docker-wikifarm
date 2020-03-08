d8287c3d5a32c3175e175aa7650e326830a2ba53
Cindy Cicalese <cindom@gmail.com>
2020-03-07 20:20:41 -0500

    fixed clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -10,5 +10,6 @@ docker image rm -f wikifarm-1.32
 docker image rm -f wikifarm-1.33
 docker image rm -f wikifarm-1.34
 docker image rm -f wikifarm-master
+docker image rm -f database
 docker container prune -f
 docker image prune -f
